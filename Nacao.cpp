9027d42031c050cb0bc9d4e701114a198868bfb7
Matheus <matheus@ubuntu.ubuntu-domain>
2013-11-06 19:14:32 -0200

    Diminuicao dos atributos da nacao quando uma unidade eh construida

--- Nacao.cpp
+++ Nacao.cpp
@@ -1,5 +1,9 @@
 #include "Nacao.h"
 
+#define AVIAO 0
+#define NAVIO 1
+#define SOLDADO 2
+#define CANHAO 3
 
 Nacao::Nacao(int _petroleo, int _madeira, int _comida, int _ferro)
 {
@@ -13,6 +17,30 @@ Nacao::Nacao(int _petroleo, int _madeira, int _comida, int _ferro)
 void Nacao::exercitoAdd(Unidade* unidade)
 {
 	exercito.push_back(unidade);
+
+	if( (*unidade).tipo == AVIAO)
+	{
+		petroleo -= 200;
+		ferro -= 200;
+	}
+
+	if( (*unidade).tipo == NAVIO)
+	{
+		petroleo -= 200;
+		ferro -= 200;	
+	}	
+
+	if( (*unidade).tipo == SOLDADO)
+	{
+		comida -= 200;	
+	}	
+
+	if( (*unidade).tipo == CANHAO)
+	{
+		madeira -= 200;
+		ferro -= 200;	
+	}
+
 }
 
 void Nacao::exercitoPop()
